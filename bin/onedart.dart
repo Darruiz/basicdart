06f6e99262d6d589b8947b6633b00fb37fc66909
Darruiz <darruizhomeoffice@gmail.com>
2023-07-01 15:52:50 -0300

    config basica de dart

---
+++ bin/onedart.dart
@@ -0,0 +1,6 @@
+void main(List<String> arguments) {
+  var ter = 4;
+  var rer = 5;
+  var resul = rer + ter;
+  print('A soma de $ter + $rer é de $resul');
+}
---
+++ test/onedart_test.dart
@@ -0,0 +1,8 @@
+import 'package:onedart/onedart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
