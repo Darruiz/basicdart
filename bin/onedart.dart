7f0670441a0083bc379465284a511a98c1824708
Darruiz <darruizhomeoffice@gmail.com>
2023-07-01 22:39:23 -0300

    Add de vária

--- bin/onedart.dart
+++ bin/onedart.dart
@@ -1,6 +1,11 @@
-void main(List<String> arguments) {
+void main() {
   var ter = 4;
   var rer = 5;
   var resul = rer + ter;
-  print('A soma de $ter + $rer é de $resul');
+  var nome = 'Darruiz';
+  //String nome = 'Darruiz';
+  //int nome = 3;
+  //Object nome = 'Darruiz';
+  //dynamic nome = 'Darruiz'; //Dynamic é uma VÁRIAVEL dinamica que permite alterar valor facilmente após definição
+  print('A soma de $ter + $rer é de $resul meu nome é $nome');
 }
