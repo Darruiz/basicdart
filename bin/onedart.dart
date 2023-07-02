51d47b557ed0f6af8703e34bf893ba2e3197b0f8
Darruiz <darruizhomeoffice@gmail.com>
2023-07-01 22:50:53 -0300

    Add de Default value

--- bin/onedart.dart
+++ bin/onedart.dart
@@ -1,11 +1,27 @@
 void main() {
-  var ter = 4;
-  var rer = 5;
-  var resul = rer + ter;
-  var nome = 'Darruiz';
+  //Init em variáveis
+
+  //var ter = 4;
+  //var rer = 5;
+  //var resul = rer + ter;
+  //var nome = 'Darruiz';
   //String nome = 'Darruiz';
   //int nome = 3;
   //Object nome = 'Darruiz';
   //dynamic nome = 'Darruiz'; //Dynamic é uma VÁRIAVEL dinamica que permite alterar valor facilmente após definição
-  print('A soma de $ter + $rer é de $resul meu nome é $nome');
+  //print('A soma de $ter + $rer é de $resul meu nome é $nome');
+
+  //Valor padrão/Default value
+//Algumas variáveis não podem receber valor nulo  non-nullable types
+//Quando adicionado ? ao lado do tipo então a variável pasdsa a poder receber um valor nulo/null
+  //Object? ex = null;
+  //double ex = null;
+  //num ex = null;
+  //int ex = null;
+  //String ex = null;
+  //Iterable ex = null;
+  //bool ex = null;
+  //List ex = null;
+
+  //print(ex);
 }
